9f255343546d34fd99c5f9f13244e09c79d9a70f
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-05-18 11:27:18 +0700

    menambahkan penghapusan memori untuk objek rekening di fungsi main

--- praktikum.cpp
+++ praktikum.cpp
@@ -81,5 +81,11 @@ int main() {
     rekening4->tampilSaldo();
     cout << endl;
 
-    
+    // Hapus memori
+    delete rekening1;
+    delete rekening2;
+    delete rekening3;
+    delete rekening4;
+
+    return 0;
 }
