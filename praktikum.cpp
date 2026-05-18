9995ab37d70f6b5b371523e2a3cc089be447e9ff
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-05-18 11:26:37 +0700

    menambahkan fungsi main dan pointer base class

--- praktikum.cpp
+++ praktikum.cpp
@@ -20,3 +20,48 @@ public:
     virtual ~RekeningBank() {}
 };
 
+// Rekening Syariah
+class RekeningSyariah : public RekeningBank {
+public:
+    RekeningSyariah(double s) : RekeningBank(s) {}
+
+    void potongAdmin() override {
+        cout << "Rekening Syariah: Tidak ada potongan admin." << endl;
+    }
+};
+
+// Rekening Konvensional
+class RekeningKonvensional : public RekeningBank {
+public:
+    RekeningKonvensional(double s) : RekeningBank(s) {}
+
+    void potongAdmin() override {
+        saldo -= 15000;
+        cout << "Rekening Konvensional: Potongan admin Rp 15.000" << endl;
+    }
+};
+
+// Rekening Premium
+class RekeningPremium : public RekeningBank {
+public:
+    RekeningPremium(double s) : RekeningBank(s) {}
+
+    void potongAdmin() override {
+        if (saldo > 10000000) {
+            cout << "Rekening Premium: Bebas biaya admin." << endl;
+        } else {
+            saldo -= 50000;
+            cout << "Rekening Premium: Potongan admin Rp 50.000" << endl;
+        }
+    }
+};
+
+int main() {
+
+    // Pointer base class
+    RekeningBank* rekening1 = new RekeningSyariah(5000000);
+    RekeningBank* rekening2 = new RekeningKonvensional(5000000);
+    RekeningBank* rekening3 = new RekeningPremium(8000000);
+    RekeningBank* rekening4 = new RekeningPremium(15000000);
+
+    
